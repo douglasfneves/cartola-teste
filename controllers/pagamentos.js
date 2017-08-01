e63fa77e6ef2f31981871846d48276d51d117fbc
Douglas Neves <douglas.neves@gmail.com>
2017-08-01 17:13:23 -0300

    Finalizando a Aula 2

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,10 +4,56 @@ module.exports = function(app) {
     res.send('ok');
   });
 
-  app.post('/pagamentos/pagamento', function(req, res){
+  // app.post('/pagamentos/pagamento', function(req, res){
+  //   var pagamento = req.body;
+  //   console.log('processando uma requisição de um novo pagamento');
+
+  //   pagamento.status = 'CRIADO';
+  //   pagamento.data = new Date;
+
+  //   var connection = app.persistencia.connectionFactory();
+  //   var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+  //   pagamentoDao.salva(pagamento, function(erro, resultado){
+  //     console.log('pagamento criado');
+  //     res.json(pagamento);
+  //   });
+
+  // });
+
+  app.post("/pagamentos/pagamento",function(req, res) {
+
+    req.assert("forma_de_pagamento", "Forma de pagamento é obrigatório").notEmpty();
+    req.assert("valor", "Valor é obrigatório e deve ser um decimal").notEmpty().isFloat();
+
+    var erros = req.validationErrors(); //getValidationResult();
+
+    if (erros) {
+      console.log('Erros de validação encontrado');
+      res.status(400).send(erros);
+      return
+    }
+
     var pagamento = req.body;
-    console.log(pagamento);
-    res.send('OK man');
+    console.log('processando pagamento...');
+
+    var connection = app.persistencia.connectionFactory();
+    var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+    pagamento.status = "CRIADO";
+    pagamento.data = new Date;
+
+    pagamentoDao.salva(pagamento, function(erro, resultado){
+      if (erro) {
+        console.log('Erro ao inserir no banco: ' + erro)
+        res.status(500).send(erro);
+      } else {
+        console.log('pagamento criado: ' + resultado);
+        res.location('/pagamentos/pagamento/' + resultado.insertId);
+
+        res.status(201).json(pagamento);
+      }
+    });
   });
 
 }
